d0fb99727152835bffc1813af8407c27c5ee7d06
stephen nene <stevekid705@gmail.com>
2023-09-03 13:06:52 +0300

    new codes

---
+++ pinger.sh
@@ -0,0 +1,19 @@
+# #!/bin/bash
+
+url="https://mnetimall.onrender.com"
+
+echo " "
+echo "🚀 Pinging $url..."
+echo " "
+# Use 'curl' to ping the website
+response=$(curl -s -o /dev/null -w "%{http_code}" $url)
+
+# Check the response code
+if [ "$response" == "200" ]; then
+  echo "✅ Website is up!"
+else
+  echo "❌ Website is down. Response code: $response"
+fi
+echo " "
+
+
